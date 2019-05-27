44b7a4a410fce65fffc56362a15e4e426954fdad
Matthew Hallberg <matthewhallberg@gmail.com>
2019-05-27 02:31:16 -0400

    first commit

---
+++ Assets/Scripts/NativeLibAdapter.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.InteropServices;
+
+public class NativeLibAdapter {
+#if !UNITY_EDITOR
+    /*
+        [DllImport("native-lib")]
+        private static extern int TestFunction_Internal();
+    */
+#elif UNITY_EDITOR
+    [DllImport("OpenCVPlugin")]
+    private static extern int TestFunction_Internal();
+#endif
+
+
+    public static int Test() {
+#if !UNITY_EDITOR
+            return TestFunction_Internal();
+#elif UNITY_EDITOR
+        return TestFunction_Internal();
+#else
+            return -1;
+#endif
+    }
+}
---
+++ Assets/Scripts/OpenCV.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Runtime.InteropServices;
+using System;
+
+
+public class OpenCV : MonoBehaviour {
+    void Awake() {
+    }
+
+    void Update() {
+        Debug.LogWarning("Test Value in C++ is " + NativeLibAdapter.Test());
+    }
+}
